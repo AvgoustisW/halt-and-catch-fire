efadb2d146b40de7449e4dc0c36a823186b9c84a
AvgoustisW <avgoustisw@gmail.com>
2021-09-28 18:11:25 +0300

    sm

--- pages/api/auth/login-pleo.tsx
+++ pages/api/auth/login-pleo.tsx
@@ -6,12 +6,38 @@ import userModel from '../../../models/user-pleo-space';
 import { sign } from 'jsonwebtoken';
 import cookie from 'cookie';
 
+import Cors from 'cors';
+
 const secret = process.env.JWT_SECRET
+
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+var whitelist = ['http://localhost:3000', 'https://pleo-spc.vercel.app']
+
+const cors = Cors({
+  methods: ['POST'],
+  origin: whitelist,
+  credentials: true,
+
+})
 
 export default async function login(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+
+  await runMiddleware(req, res, cors)
 
   if (req.method === 'POST') {  
     await dbConnect();
@@ -21,17 +47,14 @@ export default async function login(
       return; 
     }
     console.log('user', user);
-
-    res.setHeader("Content-Type", "application/json");
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    
+   
     compare(req.body.password, user.password, function(err, result) {
       if(!err && result) {
         const c = {sub: user._id, name: user.name};
         const jwt = sign(c, secret, {expiresIn: '1h'})
         res.setHeader("Set-Cookie", cookie.serialize("authToken", jwt, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV !== "development",
+          //httpOnly: true,
+          //secure: process.env.NODE_ENV !== "development",
           maxAge: 60*60*1000,
           expires: new Date().setHours( new Date().getTime() + 60*60*1000),
           path: '/',
